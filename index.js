2f7f855f1826e708a3f36b17dd7cdb248526eb1d
jinhyeok <ekdrn3330@naver.com>
2021-07-21 17:46:03 +0900

    test2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// modules
 const express = require('express');
 const mysql = require('mysql');
+const bodyPaser = require('body-parser');
 const app = express();
 const port = 3000;
 
-// db conn
+
+// db setting
 const conn = mysql.createConnection({
   host     : 'localhost',
   port     : 3306,
@@ -14,14 +17,34 @@ const conn = mysql.createConnection({
  
 conn.connect();
  
-// db query
-conn.query('SELECT * FROM test', function (error, results, fields) {
-  if (error) console.log(error);
-  console.log(results);
-});
- 
-conn.end();
+// other setting
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyPaser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res) => res.send('hello world'));
+
+// routes
+app.get('/', function(req, res) {
+  res.redirect('/contacts');
+});
+app.get('/contacts', function(req, res){
+  conn.find({}, function(err, rows){
+    if (err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+});
+app.post('/contacts', function(req, res){
+  
+});
+
+
+// port setting
 app.listen(port, () => console.log(`example app listening on port http://localhost:${port}`));
+
+
+
 
