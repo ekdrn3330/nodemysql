0e37ed33382d54147ba34855c2c79bc5a3e93946
jinhyeok <ekdrn3330@naver.com>
2021-07-22 18:10:37 +0900

    test1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // modules
 const express = require('express');
 const mysql = require('mysql');
-const bodyPaser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -20,26 +20,49 @@ conn.connect();
 // other setting
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyPaser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 
 // routes
-app.get('/', function(req, res) {
-  res.redirect('/contacts');
-});
-app.get('/contacts', function(req, res){
-  conn.find({}, function(err, rows){
-    if (err) return res.json(err);
-    res.render('contacts/index', {contacts:contacts});
+app.get('/', (req, res) => res.redirect('/contacts'));
+app.get('/contacts', (req, res) => res.render('contacts/index'));
+app.post('/', function(req, res) {
+  var sql = 'INSERT INTO user SET ?';
+
+  conn.query(sql, req.body, function(err, results, fields) {
+    if (err) throw err;
+    console.log(results);
+    res.redirect('/getlist');
   });
 });
-app.get('/contacts/new', function(req, res){
-  res.render('contacts/new');
+app.get('/getlist', function(req, res) {
+  var sql = "SELECT * FROM user";
+
+  conn.query(sql, function(err, results, fields) {
+    if (err) throw err;
+    res.render('contacts/getlist', {users : results});
+  });
 });
-app.post('/contacts', function(req, res){
-  
+app.get('/delete/:id', function(req, res) {
+  var sql = 'DELETE FROM user WHERE num = ?';
+
+  conn.query(sql, [req.params.id], function(err, results, fields) {
+    if (err) throw err;
+    console.log(results);
+    res.redirect('/getlist');
+  });
 });
+app.get('/update/:id', function(req, res) {
+  var sql = 'SELECT * FROM user WHERE num = ?';
+
+  conn.query(sql, [req.params.id], function(err, results, fields) {
+    if (err) throw err;
+    console.log(results);
+    res.render('contacts/update', {users : results});
+  });
+});
+
 
 
 // port setting
